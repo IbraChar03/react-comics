5ca82c7bf455c417686bfd3e51b9114445c1309a
Ibrahim Char <113177312+IbraChar03@users.noreply.github.com>
2023-02-25 21:46:20 +0100

    componente header e prima parte main

--- src/App.js
+++ src/App.js
@@ -1,14 +1,24 @@
 
-import './App.css';
+import './app.scss';
 import Header from './components/Header'
+import FirstMain from './components/FirstMain'
+import React, { Component } from "react"
 
-function App() {
-  return (
-    <>
-      <Header />
+class App extends Component {
+  render() {
 
-    </>
-  );
+    return (
+      <>
+        <header>
+          <Header />
+        </header>
+
+        <FirstMain />
+
+
+      </>
+    );
+  }
 }
 
 export default App;
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,15 +1,76 @@
-function Header() {
-    return (
-        <div >
+import React, { Component } from "react"
+import '../app.scss';
+class Header extends Component {
+    links = [
+        {
+            name: "CHARACTERS",
+            href: "#",
+        },
+        {
+            name: "COMICS",
+            href: "#",
+        },
+        {
+            name: "MOVIES",
+            href: "#",
+        },
+        {
+            name: "TV",
+            href: "#",
+        },
+        {
+            name: "GAMES",
+            href: "#",
+        },
+        {
+            name: "COLLECTIBLES",
+            href: "#",
+        },
+        {
+            name: "VIDEOS",
+            href: "#",
+        },
+        {
+            name: "FANS",
+            href: "#",
+        },
+        {
+            name: "NEWS",
+            href: "#",
+        },
+        {
+            name: "SHOP",
+            href: "#",
+        },
+    ]
+    render() {
 
 
-            <p>
-                Header
-            </p>
+
+        return (
+            <header>
+                <div className="cont-header">
 
 
-        </div>
-    );
+                    <ul>
+                        {this.links.map((link, index) => (
+                            <li className="list-header" key={index}>
+                                <a href="">
+                                    {link.name}
+                                </a>
+
+                            </li>
+                        )
+
+                        )}
+
+                    </ul>
+
+                </div>
+
+            </header>
+        );
+    }
 }
 
 export default Header;
